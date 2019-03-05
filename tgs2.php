c1c2c7a8cff1a128be4ef4efddcf8f3692d2aa7f
indridwi <47770537+indridwi@users.noreply.github.com>
2019-03-05 13:21:16 +0700

    Add files via upload

---
+++ tgs2.php
@@ -0,0 +1,19 @@
+<?php
+    $organisasi     array(
+     "keagamaan"    array(
+        "ketua"     "siti",
+        "wakil"     "maryam",
+        "anggota"      "husni"
+     ),
+    "kebersihan"    array(
+        "ketua"         "aditya",
+        "wakil"         "hj",
+        "sekretaris"     "chulis"
+    ),
+    "kemanusiaan"   array(
+        "ketua"     "pratama",
+        "wakil"     "nugraha"
+     
+    )
+); 
+?>
